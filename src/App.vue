4071d7794efa38e2d3309a22c905b083ed938c56
unknown <1298868344@qq.com>
2023-03-22 21:38:50 +0800

    2023年3月22日21:38:48

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <a-button type="primary">Primary</a-button>
+  <div id="app">
+    <div class="PickImage">
+
+    </div>
+    <div class="Picklist">
+
+    </div>
+    <div class="TagProcessing">
+      
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #F7F7F7;
+}
+
+.PickImage{
+  
 }
 </style>
