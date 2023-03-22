7cf1a08ceb9256299452394c0ef1f7d2c008c5a4
unknown <1298868344@qq.com>
2023-03-23 00:34:19 +0800

    2023年3月23日00:34:17

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <div id="app">
     <div class="PickImage">
-
+      <!-- 图片选择 -->
     </div>
+
     <div class="Picklist">
+      <!-- 列表选择 -->
+      <div class="menuLeft">
+        <a-menu mode="vertical" :default-selected-keys="['图片属性']">
+          <a-menu-item v-for="item in Array_MenuLeft" :key="item">
+          {{ item }}
+          </a-menu-item>
+        </a-menu>
+      </div>
+
 
     </div>
     <div class="TagProcessing">
-      
+      <!-- 标签处理 -->
     </div>
   </div>
 </template>
@@ -16,18 +26,27 @@
 
 export default {
   name: 'App',
-  components: {
+  components: {},
+  data() {
+    return {
+      Array_MenuLeft: ['图片属性', '头发','眼睛','身体','套装','上装','下装','鞋子','首饰']
+    }
+  },
+  methods: {
 
-  }
+  },
 }
 </script>
 
 <style>
+@import './assets/css/App.css';
+
 #app {
   background-color: #F7F7F7;
-}
-
-.PickImage{
-  
+  /* flex 布局 */
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: start;
 }
 </style>
