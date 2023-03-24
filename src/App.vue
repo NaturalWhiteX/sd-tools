96f6e30f8b40c7473acf77db233bd4e02c1dd4f9
谢子文 <xieziwen@NaturalWhite--MacBook-Air.local>
2023-03-24 18:10:18 +0800

    2023年03月24日18:10:18

--- src/App.vue
+++ src/App.vue
@@ -1,98 +1,111 @@
 <template>
   <div id="app">
+    <a-page-header
+      :style="{ background: 'var(--color-bg-2)' }"
+      title="SD Tools"
+      subtitle="Stable Diffusion 提示词助手"
+      :show-back="false"
+    >
+      <template #extra>
+        <a-radio-group type="button" default-value="large">
+          <a-radio value="mini">Mini</a-radio>
+          <a-radio value="small">Small</a-radio>
+          <a-radio value="large">Large</a-radio>
+        </a-radio-group>
+      </template>
+    </a-page-header>
     <!-- 图示选择 -->
     <div class="PickImage"></div>
 
-    <a-row justify="center" gutter="24">
-      <a-col class="Picklist" :span="12" style="padding-left: 0px">
-        <!-- 列表选择 -->
-        <a-row :wrap="false">
-          <a-col flex="80px" class="menuLeft">
-            <a-menu
-              mode="vertical"
-              :default-selected-keys="[ArrayForMenu.ArrayMenu[0].menuId]"
-              :selected-keys="menuSelected"
+    <div class="pageContainer">
+      <div class="Picklist">
+        <!-- 菜单 -->
+        <div class="menuLeft">
+          <a-menu
+            mode="vertical"
+            :default-selected-keys="[ArrayForMenu.ArrayMenu[0].menuId]"
+            :selected-keys="menuSelected"
+          >
+            <!-- 菜单项 -->
+            <a-menu-item
+              v-for="(item, index) in ArrayForMenu.ArrayMenu"
+              :key="item.menuId"
+              @click="MenuClick(index)"
             >
-              <!-- 菜单项 -->
-              <a-menu-item
-                v-for="(item, index) in ArrayForMenu.ArrayMenu"
-                :key="item.menuId"
-                @click="MenuClick(index)"
+              {{ item.menuName }}
+            </a-menu-item>
+          </a-menu>
+        </div>
+
+        <div class="PickMain">
+          <!-- 子菜单 -->
+          <div class="tabsChildMenu">
+            <a-space size="large">
+              <!-- 子菜单项 -->
+              <div
+                class="childMenuItem"
+                v-for="(item, index) in ArrayChildMenu"
+                :key="item.childMenuId"
+                @click="ChildMenuClick(index)"
               >
-                {{ item.menuName }}
-              </a-menu-item>
-            </a-menu>
-          </a-col>
-          <a-col flex="auto" class="PickMain">
-            <h2>标签选购区</h2>
-            <a-alert
-              >操作说明：点击便签复选框将标签选入暂存箱，点击标签尾部图标复制到剪切板</a-alert
-            >
-            <!-- 标签页 -->
-            <a-tabs
-              :default-active-key="ArrayChildMenu[0].childMenuId"
-              v-model:active-key="childMenuSelected"
-              @change="ChildMenuClick()"
-            >
-              <div v-for="item in ArrayChildMenu" :key="item.childMenuId">
-                <a-tab-pane
-                  :key="item.childMenuId"
-                  :title="item.childMenuName"
+                <text
+                  class="titleChildMenu cTransition"
+                  :class="{
+                    titleChildMenuActived:
+                      childMenuSelected === item.childMenuId,
+                  }"
+                  >{{ item.childMenuName }}</text
                 >
-                  <text>{{ item.childMenuId }}</text>
-                  <!-- 标签 -->
-                  <a-space wrap size="medium" style="padding-top: 12px">
-                    <template
-                      v-for="(item, indexPrompt) in ArrayPrompt"
-                      :key="indexPrompt"
-                    >
-                      <div
-                        class="promptTag customTransition"
-                        :class="{ promptTagChecked: item.isChecked === true }"
-                      >
-                        <a-space>
-                          <a-checkbox v-model="item.isChecked">
-                            <a-space>
-                              <text>{{ item.promptName }}</text>
-                              <text class="textTranslation">{{
-                                item.translation
-                              }}</text>
-                            </a-space>
-                          </a-checkbox>
-                          <a-button
-                            type="text"
-                            shape="circle"
-                            @click="
-                              () => this.$message.info('已复制此标签到剪切板')
-                            "
-                          >
-                            <template #icon>
-                              <template v-if="!item.isChecked">
-                                <icon-copy :style="{ fontSize: '16px' }" />
-                              </template>
-                              <template v-else>
-                                <icon-copy
-                                  :style="{
-                                    fontSize: '16px',
-                                    color: '#ffffff',
-                                  }"
-                                />
-                              </template>
-                            </template>
-                          </a-button>
-                        </a-space>
-                      </div>
-                    </template>
-                  </a-space>
-                </a-tab-pane>
               </div>
-            </a-tabs>
-          </a-col>
-        </a-row>
-      </a-col>
+            </a-space>
+          </div>
+          <!-- 标签 -->
+          <div class="tabsChildMenuContent">
+            <template v-for="(item, index) in ArrayPrompt" :key="index">
+              <div
+                class="promptTag cTransition"
+                :class="{ promptTagChecked: item.isChecked === true }"
+              >
+                <a-space>
+                  <a-checkbox v-model="item.isChecked" @change="checkBoxChange()">
+                    <a-space>
+                      <text>{{ item.promptName }}</text>
+                      <text class="textTranslation">{{
+                        item.promptTranslation
+                      }}</text>
+                    </a-space>
+                  </a-checkbox>
+                  <a-button
+                    type="text"
+                    shape="circle"
+                    @click="() => this.$message.info('已复制此标签到剪切板')"
+                  >
+                    <template #icon>
+                      <template v-if="!item.isChecked">
+                        <icon-copy :style="{ fontSize: '16px' }" />
+                      </template>
+                      <template v-else>
+                        <icon-copy
+                          :style="{
+                            fontSize: '16px',
+                            color: '#ffffff',
+                          }"
+                        />
+                      </template>
+                    </template>
+                  </a-button>
+                  <!-- 标签内的内容 end -->
+                </a-space>
+              </div>
+            </template>
+          </div>
+          <!-- 标签 end -->
+        </div>
+        <!-- pickMain end -->
+      </div>
 
       <!-- 标签处理 -->
-      <a-col class="TagProcessing" :span="10">
+      <div class="TagProcessing">
         <div class="mainContent">
           <a-space direction="vertical" fill>
             <h2>标签暂存箱</h2>
@@ -100,7 +113,16 @@
               <text>编辑权重</text>
               <a-switch disabled="" />
             </a-space>
-            <div class="tagBoxArea"></div>
+            <div class="tagBoxArea">
+              <a-space size="medium" :wrap="true">
+                <template
+                  v-for="(item, index) in ArrayPromptSelected"
+                  :key="index"
+                >
+                  <a-tag closable>{{item.promptName}}</a-tag>
+                </template>
+              </a-space>
+            </div>
             <div class="formatButtonContent">
               <text
                 >“黄色标签”为当前系统内暂未收录的标签，“一键收录”将收录这些黄色标签。</text
@@ -110,7 +132,7 @@
           </a-space>
           <a-space class="generateTags" direction="vertical" fill>
             <a-space class="generateTagsTitle">
-              <h2>生成标签</h2>
+              <h2>生成提示词</h2>
               <a-space size="medium">
                 <a-button>清空</a-button>
                 <a-button
@@ -133,8 +155,9 @@
             </div>
           </a-space>
         </div>
-      </a-col>
-    </a-row>
+      </div>
+    </div>
+    <!-- pageContainer end -->
   </div>
 </template>
 
@@ -164,53 +187,41 @@ export default {
           promptTranslation: "",
         },
       ],
-
-      Array_Tag: [
-        {
-          name: "masterpiece",
-          translation: "杰作",
-          isChecked: true,
-        },
-        {
-          name: "best quality",
-          translation: "最好的质量",
-          isChecked: false,
-        },
-        {
-          name: "extremely detailed CG unity 8k wallpaper",
-          translation: "最高质量",
-          isChecked: false,
-        },
-      ],
+      ArrayPromptSelected: [],
     };
   },
   mounted() {
     let that = this;
     that.filterChildMenu();
     that.filterPrompt();
-    console.log("ArrayChildMenu", that.ArrayChildMenu);
-    console.log("ArrayPrompt", that.ArrayPrompt);
   },
   methods: {
     MenuClick(index) {
+      // 菜单点击
       let that = this;
       that.menuSelected = that.ArrayForMenu.ArrayMenu[index].menuId;
       that.filterChildMenu();
+      if (that.ArrayChildMenu.length === 0) {
+        return;
+      }
+      that.childMenuSelected = that.ArrayChildMenu[0].childMenuId;
+      that.filterPrompt();
     },
-    ChildMenuClick() {
+    ChildMenuClick(index) {
+      // 子菜单点击
       let that = this;
-      let tempStr = that.childMenuSelected.substring(1);
-      let index = parseInt(tempStr) - 1;
       that.childMenuSelected = that.ArrayChildMenu[index].childMenuId;
       that.filterPrompt();
     },
     filterChildMenu() {
+      // 根据 菜单列表 过滤 子菜单列表
       let that = this;
       that.ArrayChildMenu = that.ArrayForMenu.ArrayChildMenu.filter((value) => {
         return value.belongMenuId === that.menuSelected ? true : false;
       });
     },
     filterPrompt() {
+      // 根据 子菜单列表 过滤 提示词列表
       let that = this;
       that.ArrayPrompt = that.ArrayForMenu.ArrayPrompt.filter((value) => {
         return value.belongChildMenuID === that.childMenuSelected
@@ -218,6 +229,18 @@ export default {
           : false;
       });
     },
+    checkBoxChange(){
+      let that = this;
+      let ArrayPromptSelected = []; 
+      ArrayPromptSelected = that.ArrayPrompt.filter((value)=>{
+        let result = false;
+        if(value.isChecked && value.isChecked === true){
+          result = true;
+        }
+        return result;
+      })
+      that.ArrayPromptSelected = ArrayPromptSelected;
+    }
   },
 };
 </script>
@@ -233,13 +256,10 @@ h2 {
   color: #333333;
 }
 
-.arco-row {
-  margin-left: 0 !important;
-  margin-right: 0 !important;
+text {
+  line-height: 24px;
 }
-.arco-col {
-  padding-right: 12px !important;
-}
+
 .arco-menu-item {
   padding-left: 24px !important;
 }
@@ -252,20 +272,6 @@ h2 {
 .arco-menu-inner {
   /* 让菜单左侧无间距 */
   padding: 4px 0 !important;
-}
-
-.arco-tabs-content .arco-tabs-content-list {
-  /* 换行问题 */
-  display: block !important;
-}
-
-.arco-tabs-tab-active .arco-tabs-tab-title {
-  font-size: 16px;
-  font-weight: bold;
-}
-
-.arco-tabs-nav-ink {
-  height: 4px !important;
 }
 
 .arco-btn-text:hover {
@@ -295,7 +301,7 @@ h2 {
 
 /* ========== 动画 ========== */
 
-.customTransition {
+.cTransition {
   /* 工具 class */
   transition: all 0.25s;
 }
